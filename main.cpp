79486364ee600038903c1ddac847df05379890df
Ryangregoire286-star <ryangregoire286@gmail.com>
2026-03-23 11:58:36 -0400

    added code for uppercase

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <Windows.h>
 #include <string>
-
+#include <tuple>
+#include <algorithm>
+#include <cctype>
 using namespace std;
 
 class Command {
@@ -23,7 +25,19 @@ public:
 	}
 };
 
+static void PrintTitle(string title) {
+	std::cout << title << std::endl;
+}
+
 int main() {
+
+
+	const auto [x, y, z] = std::make_tuple("Add", "Commit", "Push");
+
+	string title = "Git-Hub Command Parser";
+
+	std::transform(title.begin(), title.end(), title.begin(), ::toupper);
+	PrintTitle(title);
 
 	Command command("");
 	std::cout << "Enter Type Command: ";
@@ -32,31 +46,40 @@ int main() {
 	cin >> commands;
 
 
-	if (commands.c_str() == "Add") {
+	if (commands.c_str() == x) {
 
 		std::cout << "Enter Type Command: ";
 		string addCommands;
+		std::transform(addCommands.begin(), addCommands.end(), addCommands.begin(), ::toupper);
+		PrintTitle(addCommands);
 		cin >> addCommands;
+
 
 		ShellExecuteA(0, "open", addCommands.c_str(), NULL, NULL, SW_SHOWDEFAULT);
 	}
 
-	if (commands.c_str() == "Commit") {
+	if (commands.c_str() == y) {
 
 		std::cout << "Enter Commit Command: ";
 		string commitCommands;
+		std::transform(commitCommands.begin(), commitCommands.end(), commitCommands.begin(), ::toupper);
+		PrintTitle(commitCommands);
 		cin >> commitCommands;
+
 
 		ShellExecuteA(0, "open", commitCommands.c_str(), NULL, NULL, SW_SHOWDEFAULT);
 
 	}
 
 
-	if (commands.c_str() == "Push") {
+	if (commands.c_str() == z) {
 
 		std::cout << "Enter Push Command: ";
 		string pushCommands;
+		std::transform(pushCommands.begin(), pushCommands.end(), pushCommands.begin(), ::toupper);
+		PrintTitle(pushCommands);
 		cin >> pushCommands;
+
 
 
 		ShellExecuteA(0, "open", pushCommands.c_str(), NULL, NULL, SW_SHOWDEFAULT);
