7a02ca52e6aa45aad746ef99bd226ae862ece44d
Ryangregoire286-star <ryangregoire286@gmail.com>
2026-03-22 22:55:22 -0400

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+	std::cout << "Enter Type Command: ";
+	string commands;
+	cin >> commands;
+
+
+	if (commands.c_str() == "Add") {
+
+		std::cout << "Enter Type Command: ";
+		string addCommands;
+		cin >> addCommands;
+
+		ShellExecuteA(0, "open", addCommands.c_str(), NULL, NULL, SW_SHOWDEFAULT);
+	}
+
+	if (commands.c_str() == "Commit") {
+
+		std::cout << "Enter Commit Command: ";
+		string commitCommands;
+		cin >> commitCommands;
+
+		ShellExecuteA(0, "open", commitCommands.c_str(), NULL, NULL, SW_SHOWDEFAULT);
+
+	}
+
+
+	if (commands.c_str() == "Push") {
+
+		std::cout << "Enter Push Command: ";
+		string pushCommands;
+		cin >> pushCommands;
+
+
+		ShellExecuteA(0, "open", pushCommands.c_str(), NULL, NULL, SW_SHOWDEFAULT);
+
+	}
+
+}
